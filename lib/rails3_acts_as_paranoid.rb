1171270d016b15fe8b28e762ba97cc7a191036fe
Donald Piret <donald@donaldpiret.com>
2012-03-20 17:49:04 +0800

    Don't use default_scope but instead define a scope active on every model

--- lib/rails3_acts_as_paranoid.rb
+++ lib/rails3_acts_as_paranoid.rb
@@ -36,7 +36,8 @@ module ActsAsParanoid
     include ActsAsParanoid::Associations
     
     # Magic!
-    default_scope { where(paranoid_default_scope_sql) }
+    #default_scope { where(paranoid_default_scope_sql) }
+    scope :active, { where(paranoid_default_scope_sql) }
 
     if paranoid_configuration[:column_type] == 'time'
       scope :deleted_inside_time_window, lambda {|time, window|
